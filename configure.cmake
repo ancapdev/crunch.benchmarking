dd38793cb8f158b1ae8e76273a8a03378baeed1d
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-20 22:57:26 +0000

    Added custom targets for building and running all benchmarks

--- configure.cmake
+++ configure.cmake
@@ -8,8 +8,23 @@ vpm_depend(
 vpm_include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
 macro(crunch_add_benchmark _name)
+  # Shared build target for all benchmarks
+  if(NOT TARGET build-benchmarks)
+    add_custom_target(build-benchmarks)
+  endif()
+
+  # Shared run target for all benchmarks
+  if(NOT TARGET run-benchmarks)
+    add_custom_target(run-benchmarks)
+  endif()
+
+  # Run target for this benchmark
   add_custom_target("run-${_name}"
     ${_name}
     WORKING_DIRECTORY ${CMAKE_CURRENTY_BINARY_DIR}
     VERBATIM)
+
+  # Add dependencies on shared build and run targets
+  add_dependencies(build-benchmarks ${_name})
+  add_dependencies(run-benchmarks "run-${_name}")
 endmacro()
